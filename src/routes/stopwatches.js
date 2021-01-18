b7ee9bb87c75f69566bf633b7c3d41aadeda871b
Erica <ericasu33@gmail.com>
2021-01-18 02:14:09 +0000

    modify stopwatches route

--- src/routes/stopwatches.js
+++ src/routes/stopwatches.js
@@ -4,8 +4,25 @@ const router = express.Router();
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-
+    db.query(
+      `
+        SELECT entries.id AS entry_ID, categories.name AS category_name, categories.color, tags.tag AS tag_name
+        FROM entries
+        JOIN categories
+        ON categories.id = entries.category_id
+        JOIN entries_tags
+        ON entries_tags.entries_id = entries.id
+        JOIN tags
+        on entries_tags.tag_id = tags.id
+        ORDER BY entry_id
+      `
+    ).then(data => {
+      console.log(data);
+      res.json(data);
+    });
   });
 
+
   return router;
+
 };
