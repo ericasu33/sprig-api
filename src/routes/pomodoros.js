9977877c19a7d2dee7bfac0c43cbd0748484f9d0
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-24 18:37:03 -0800

    Change pomodoro get route to get specific fields

--- src/routes/pomodoros.js
+++ src/routes/pomodoros.js
@@ -5,7 +5,17 @@ module.exports = (db) => {
 
   router.get("/", (req, res) => {
     const query1 = `
-      SELECT * 
+      SELECT id, 
+        user_id, 
+        name, 
+        work, 
+        short_break, 
+        num_repeats as cycles, 
+        long_break, 
+        shortbr_start_audio_alert_id as short_b_start_sound, 
+        shortbr_end_audio_alert_id as short_b_end_sound, 
+        longbr_start_audio_alert_id as long_b_start_sound, 
+        longbr_end_audio_alert_id as long_b_end_sound
       FROM timers
     `;
 
@@ -17,27 +27,6 @@ module.exports = (db) => {
     Promise.all([db.query(query1), db.query(query2)])
       .then(([timersData, audiosData]) => {
         const timers = timersData.rows;
-        const audios = audiosData.rows;
-
-        for (const timer of timers) {
-          for (const audio of audios) {
-            if (timer.shortbr_start_audio_alert_id === audio.id) {
-              timer.shortbr_start_audio_alert_id = audio.file_name;
-            }
-
-            if (timer.shortbr_end_audio_alert_id === audio.id) {
-              timer.shortbr_end_audio_alert_id = audio.file_name;
-            }
-
-            if (timer.longbr_start_audio_alert_id === audio.id) {
-              timer.longbr_start_audio_alert_id = audio.file_name;
-            }
-            
-            if (timer.longbr_end_audio_alert_id === audio.id) {
-              timer.longbr_end_audio_alert_id = audio.file_name;
-            }
-          }
-        }
         res.json(timers);
       });
   });
